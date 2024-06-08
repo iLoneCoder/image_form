4b6e943fe19be8eb951d4a3cb79bec5787fa64ab
giorgiShagidze <gio.shagidze@gmail.com>
2024-06-09 01:59:24 +0400

    remove content-type from request headers

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ form.addEventListener("submit", handleSubmit)
 async function handleSubmit(e) {
     e.preventDefault()
     const bodyFormat = new FormData()
-    // const myHeaders = new Headers();
-    // myHeaders.append("Content-Type", "multipart/form-data");
-
     const file = document.getElementById("image").files[0]
 
     bodyFormat.append("name", document.getElementById("model").value)   
@@ -17,9 +14,7 @@ async function handleSubmit(e) {
     try {
         const response = await fetch("https://ez3vlribw0.execute-api.us-east-1.amazonaws.com/v1", {
             method: "POST",
-            headers: {
-                "Content-type": "multipart/form-data"
-            },
+
             body: bodyFormat
         })
 
